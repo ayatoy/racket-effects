24a7efee8e4018bea7eb9a50486c9bdd07c58dd7
ayatoy <ayatoy@gmail.com>
2019-06-02 00:27:50 +0900

    add original code to examples/probability.rkt

--- examples/probability.rkt
+++ examples/probability.rkt
@@ -3,8 +3,24 @@
 (require "../effects.rkt")
 
 (struct random-real ())
+
+(define-handler default
+  [effect e k
+    [(random-real? e) (k (random))]])
+
+;;
+;; effect Toss : float -> bool
+;;
 (struct toss (probability))
 
+;;
+;; let rec uniform = function
+;;   | [x] -> x
+;;   | x :: xs ->
+;;     let n = length xs + 1 in
+;;     let p = 1.0 /. float_of_int n in
+;;     if perform (Toss p) then x else uniform xs
+;;
 (define (uniform lst)
   (if (null? (cdr lst))
       (car lst)
@@ -14,10 +30,13 @@
             (car lst)
             (uniform (cdr lst))))))
 
-(define-handler default
-  [effect e k
-    [(random-real? e) (k (random))]])
-
+;;
+;; let random_value = handler
+;;   | v -> v
+;;   | effect (Toss p) k ->
+;;     let toss = perform (RandomFloat 1.) < p in
+;;     continue k toss
+;;
 (define-handler random-value
   [value v v]
   [effect e k
@@ -26,6 +45,12 @@
                  (toss-probability e))])
        (k t))]])
 
+;;
+;; let expectation = handler
+;;   | v -> v
+;;   | effect (Toss p) k ->
+;;     p *. (continue k true) +. (1.0 -. p) *. (continue k false)
+;;
 (define-handler expectation
   [value v v]
   [effect e k
@@ -34,11 +59,29 @@
        (+ (* p (k #t))
           (* (- 1.0 p) (k #f))))]])
 
+;;
+;; with expectation handle
+;;   let x = uniform [1.; 2.; 3.; 4.; 5.; 6.] in
+;;   let y = uniform [1.; 2.; 3.; 4.; 5.; 6.] in
+;;   x +. y
+;;
 (handle-with expectation
   (let ([x (uniform '(1 2 3 4 5 6))]
         [y (uniform '(1 2 3 4 5 6))])
     (+ x y)))
 
+;;
+;; let combine p dist1 dist2 =
+;;   let scale p dist = map (fun (x, q) -> (x, p *. q)) dist in
+;;   let rec add (x, p) = function
+;;     | [] -> [(x, p)]
+;;     | (y, q) :: dist ->
+;;       if x = y then (x, p +. q) :: dist else (y, q) :: add (x, p) dist
+;;   in
+;;   let dist1 = scale p dist1 in
+;;   let dist2 = scale (1.0 -. p) dist2 in
+;;   fold_right add dist1 dist2
+;;
 (define (combine p dist1 dist2)
   (let* ([scale (lambda (p dist)
                   (map (lambda (v)
@@ -60,6 +103,11 @@
                                     (add v dist))))))])
       (foldr add dist2 dist1))))
 
+;;
+;; let distribution = handler
+;;   | v -> [(v, 1.0)]
+;;   | effect (Toss p) k -> combine p (continue k true) (continue k false)
+;;
 (define-handler distribution
   [value v (list (vector v 1.0))]
   [effect e k
@@ -68,6 +116,12 @@
               (k #t)
               (k #f))]])
 
+;;
+;; with distribution handle
+;;   let x = uniform [1; 2; 3; 4; 5; 6] in
+;;   let y = uniform [1; 2; 3; 4; 5; 6] in
+;;   x + y
+;;
 (handle-with distribution
   (let ([x (uniform '(1 2 3 4 5 6))]
         [y (uniform '(1 2 3 4 5 6))])
